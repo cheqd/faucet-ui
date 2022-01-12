e1faa4018fda638ca377ab4276c30634bed6208d
Tasos <50984242+Eengineer1@users.noreply.github.com>
2022-01-13 00:11:52 +0200

    Latest revision - functionality & branding

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,89 +1,22 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
+  <v-app class="vappbackground">
+    <img class="mainimglogo" src="../assets/img/cheqd-Logo-White.png" alt="logo"/>
     <v-main>
-      <v-container>
+      <v-container class="maincontainer">
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
-      :absolute="!fixed"
-      app
+      class="pagefooter"
+      relative
     >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <div class="social-links">
+        <a href="https://t.me/cheqd" class="social-links__icon" target="_blank"><span class="social-links__icon"><svg version="1.0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" aria-labelledby="telegram1" role="img"><title id="telegram1">Follow us on Instagram</title><g id="telegram1_layer"><path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm121.8 169.9l-40.7 191.8c-3 13.6-11.1 16.9-22.4 10.5l-62-45.7-29.9 28.8c-3.3 3.3-6.1 6.1-12.5 6.1l4.4-63.1 114.9-103.8c5-4.4-1.1-6.9-7.7-2.5l-142 89.4-61.2-19.1c-13.3-4.2-13.6-13.3 2.8-19.7l239.1-92.2c11.1-4 20.8 2.7 17.2 19.5z"></path></g></svg></span></a>
+        <a href="https://twitter.com/cheqd_io" class="social-links__icon" target="_blank"><span class="social-links__icon"><svg version="1.0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-labelledby="twitter2" role="img"><title id="twitter2">Follow us on Twitter</title><g id="twitter2_layer"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></g></svg></span></a>
+        <a href="https://www.linkedin.com/company/cheqd-identity" class="social-links__icon" target="_blank"><span class="social-links__icon"><svg version="1.0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" aria-labelledby="linkedin-in3" role="img"><title id="linkedin-in3">Follow us on LinkedIn</title><g id="linkedin-in3_layer"><path d="M100.3 480H7.4V180.9h92.9V480zM53.8 140.1C24.1 140.1 0 115.5 0 85.8 0 56.1 24.1 32 53.8 32c29.7 0 53.8 24.1 53.8 53.8 0 29.7-24.1 54.3-53.8 54.3zM448 480h-92.7V334.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V480h-92.8V180.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V480z"></path></g></svg></span></a>
+        <a href="https://github.com/cheqd" class="social-links__icon" target="_blank"><span class="social-links__icon"><svg version="1.0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 480 512" aria-labelledby="github-alt4" role="img"><title id="github-alt4">Follow us on Facebook</title><g id="github-alt4_layer"><path d="M186.1 328.7c0 20.9-10.9 55.1-36.7 55.1s-36.7-34.2-36.7-55.1 10.9-55.1 36.7-55.1 36.7 34.2 36.7 55.1zM480 278.2c0 31.9-3.2 65.7-17.5 95-37.9 76.6-142.1 74.8-216.7 74.8-75.8 0-186.2 2.7-225.6-74.8-14.6-29-20.2-63.1-20.2-95 0-41.9 13.9-81.5 41.5-113.6-5.2-15.8-7.7-32.4-7.7-48.8 0-21.5 4.9-32.3 14.6-51.8 45.3 0 74.3 9 108.8 36 29-6.9 58.8-10 88.7-10 27 0 54.2 2.9 80.4 9.2 34-26.7 63-35.2 107.8-35.2 9.8 19.5 14.6 30.3 14.6 51.8 0 16.4-2.6 32.7-7.7 48.2 27.5 32.4 39 72.3 39 114.2zm-64.3 50.5c0-43.9-26.7-82.6-73.5-82.6-18.9 0-37 3.4-56 6-14.9 2.3-29.8 3.2-45.1 3.2-15.2 0-30.1-.9-45.1-3.2-18.7-2.6-37-6-56-6-46.8 0-73.5 38.7-73.5 82.6 0 87.8 80.4 101.3 150.4 101.3h48.2c70.3 0 150.6-13.4 150.6-101.3zm-82.6-55.1c-25.8 0-36.7 34.2-36.7 55.1s10.9 55.1 36.7 55.1 36.7-34.2 36.7-55.1-10.9-55.1-36.7-55.1z"></path></g></svg></span></a>
+      </div>
+      <p style="text-align: center; font-size: 18px;">&copy; {{ new Date().getFullYear() }} cheqd - The network for authentic data | <a href="https://www.cheqd.io/privacy-policy?hsLang=en" rel="noopener" target="_blank">Privacy policy</a></p>
     </v-footer>
   </v-app>
 </template>
@@ -93,26 +26,94 @@ export default {
   name: 'DefaultLayout',
   data () {
     return {
-      clipped: false,
-      drawer: false,
       fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'cheqd Testnet faucet'
     }
   }
 }
 </script>
+<style scoped>
+  .vappbackground {
+    background: url('../assets/img/Orange_Mobius_06.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
+    font-family: 'Nunito', sans-serif;
+  }
+  .maincontainer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    padding-bottom: 50px;
+  }
+  .mainimglogo {
+    width:150px;
+    padding:30px 0 0 20px;
+    display: block;
+  }
+  .pagefooter {
+    width: 100%;
+    display: block;
+    background: rgba(48,48,48,0.8);
+  }
+  .social-links {
+    width: 100%;
+    align-items: center;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  .social-links__icon {
+      background-color: transparent;
+      border-radius: 4px;
+      display: inline-block;
+      margin: 0 0.7rem 0.35rem 0;
+      padding: 0.7rem 0.7rem 0.2rem 0.7rem;
+      cursor: pointer;
+  }
+  .social-links svg {
+    fill: rgba(255, 255, 255,1.0);
+    width: 40px;
+    height: 40px;
+    transition:fill 0.2s ease-in-out ;
+  }
+  .social-links svg:hover {
+    fill: #FE5000;
+  }
+  .sociallinks {
+      font-weight: 300;
+      text-decoration: underline;
+      font-family: Nunito,sans-serif;
+      font-style: normal;
+      color: #00c6ab;
+  }
+  @media only screen and (max-width: 992px) {
+    .social-links svg {
+      width: 30px;
+      height: 30px;
+    }
+  }
+  @media only screen and (max-width: 756px) {
+    .mainimglogo{
+      width: 150px;
+      margin: 0 auto;
+    }
+   }
+  @media only screen and (max-width: 576px) {
+    .bgdarkopacity {
+      width: 100%;
+      min-width: auto;
+    }
+    .social-links__icon {
+      margin:0;
+    }
+  }
+  @media only screen and (max-width: 480px) {
+    .social-links__icon {
+      padding: 0.7rem 0.4rem 0.2rem 0.4rem;
+    }
+  }
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -59,7 +59,7 @@ export default {
       dark: true,
       themes: {
         dark: {
-          primary: colors.blue.darken2,
+          primary: '#FE5000',
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
