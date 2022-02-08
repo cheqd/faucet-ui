d228f41c55d75df90b7accba7bd45bbc350a988b
Tasos <50984242+Eengineer1@users.noreply.github.com>
2022-02-08 21:02:31 +0100

    Added 5mn CHEQ defaults

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,4 @@
 export const CHEQD_FAUCET_SERVER = 'https://faucet-api.cheqd.io'
 export const CHEQD_MINIMAL_DENOM = 'ncheq'
-export const CHEQD_CURRENT_AMOUNT_GIVEN = '1000000'
+export const CHEQD_CURRENT_AMOUNT_GIVEN = '5000000000000000'
 export const DEFAULT_TESTING_ADDRESS = 'cheqd12248whff96tpfyqm2vyvf9k4wda9h2dhdkf2e4'
