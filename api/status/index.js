02017c85b35a049e3150d8ece82e932ea8b4efdf
Tasos <50984242+Eengineer1@users.noreply.github.com>
2022-02-11 00:30:17 +0100

    Added general faucet info endpoint

---
+++ api/status/index.js
@@ -0,0 +1,26 @@
+const app = require('express')()
+const bodyParser = require('body-parser')
+const fetch = require('node-fetch')
+import { CHEQD_FAUCET_SERVER, CHEQD_CURRENT_AMOUNT_GIVEN } from '../../constants/constants'
+
+app.use(bodyParser.json())
+
+app.all('/', async (req, res) => {
+  const ping = await fetch( `${CHEQD_FAUCET_SERVER}/status` )
+  const info = await ping.json()
+
+  const processed = {
+    overall_balance: {
+      holder: {
+        address: info.holder.address,
+        balance: info.holder.balance,
+      },
+      distributors: info.distributors
+    },
+    refill_needed: !( Math.floor( info.holder.balance / CHEQD_CURRENT_AMOUNT_GIVEN ) > 1 ) && info.distributors.filter( (v) => { return Math.floor( v.balance[0].amount / CHEQD_CURRENT_AMOUNT_GIVEN ) > 1 } ).length < 2
+  }
+
+  return res.json( processed )
+})
+
+module.exports = app
